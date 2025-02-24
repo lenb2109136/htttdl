0a11fed41a408df34551f7e27669ac936b03f531
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:21 +0700

    Create DonHangBuuCucRepository.java

---
+++ src/main/java/com/example/htttdl/repository/DonHangBuuCucRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.DonHangBuuCuc;
+
+public interface DonHangBuuCucRepository extends JpaRepository<DonHangBuuCuc, Integer> {
+
+}
