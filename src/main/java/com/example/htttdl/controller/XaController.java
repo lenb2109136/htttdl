8f3e0032c8f1be8cc679e6fbbe0ae783971d7985
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 16:34:03 +0700

    Create XaController.java

---
+++ src/main/java/com/example/htttdl/controller/XaController.java
@@ -0,0 +1,25 @@
+package com.example.htttdl.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.htttdl.modal.Xa;
+import com.example.htttdl.response.response;
+import com.example.htttdl.service.XaService;
+
+@RestController
+@RequestMapping("xa")
+public class XaController {
+	@Autowired
+	private XaService xaService;
+	@GetMapping("/getxabyhuyen")
+	public ResponseEntity<response> getXaByHuyen(int id){
+		return new ResponseEntity<response>(new response(HttpStatus.OK,"ok",xaService.getXaByHuyen(id)),HttpStatus.OK);
+	}
+}
