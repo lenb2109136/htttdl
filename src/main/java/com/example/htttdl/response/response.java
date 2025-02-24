cab6303a16f25c3c0aa779120ac9f5df702fe987
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 15:31:41 +0700

    Create response.java

---
+++ src/main/java/com/example/htttdl/response/response.java
@@ -0,0 +1,39 @@
+package com.example.htttdl.response;
+
+import org.springframework.http.HttpStatus;
+
+public class response {
+	private HttpStatus status;
+	private String message;
+	private Object data;
+	
+	public response(HttpStatus status, String message, Object l) {
+        this.status = status;
+        this.message = message;
+        this.data = l;
+    }
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
