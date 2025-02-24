f661efd8df8278988b34b827b8dea8e460bb688e
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 09:17:06 +0700

    Initial commit

---
+++ src/main/java/com/example/htttdl/HtttdlApplication.java
@@ -0,0 +1,13 @@
+package com.example.htttdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtttdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtttdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/htttdl/HtttdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htttdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtttdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
