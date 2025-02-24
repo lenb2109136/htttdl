d232d83929d9a7151eff67872189481019ee1296
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 16:34:14 +0700

    Create LoaiHangService.java

---
+++ src/main/java/com/example/htttdl/service/LoaiHangService.java
@@ -0,0 +1,20 @@
+package com.example.htttdl.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.htttdl.modal.LoaiHang;
+import com.example.htttdl.repository.LoaiHangRepository;
+
+@Service
+public class LoaiHangService {
+	@Autowired
+	private LoaiHangRepository loaiHangRepository;
+	
+	public List<LoaiHang> getall(){
+		return loaiHangRepository.findAll();
+	}
+}
