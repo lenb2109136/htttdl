43bb52f89fc8c5ecc4120a5be73148f94e6c561b
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 16:34:09 +0700

    Update HuyenRepository.java

--- src/main/java/com/example/htttdl/repository/HuyenRepository.java
+++ src/main/java/com/example/htttdl/repository/HuyenRepository.java
@@ -1,10 +1,15 @@
 package com.example.htttdl.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.htttdl.modal.Huyen;
 @Repository
 public interface HuyenRepository extends JpaRepository<Huyen, Integer>{
-
+	@Query(value ="SELECT * FROM quanhuyen WHERE t_id=:id", nativeQuery = true)
+	public List<Huyen> getHuyenByTinh(@Param("id") int id);
 }
