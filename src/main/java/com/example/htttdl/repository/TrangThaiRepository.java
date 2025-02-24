e0e3c005063fd956476a1c5e216b37606c77194e
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:53 +0700

    Create TrangThaiRepository.java

---
+++ src/main/java/com/example/htttdl/repository/TrangThaiRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.TrangThai;
+
+public interface TrangThaiRepository extends JpaRepository<TrangThai, Integer> {
+
+}
