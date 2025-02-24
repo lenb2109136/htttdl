82933b63e7ce940730cff65835a765673bf72870
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:23 +0700

    Create DonGiaRepository.java

---
+++ src/main/java/com/example/htttdl/repository/DonGiaRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.DonGia;
+
+public interface DonGiaRepository extends JpaRepository<DonGia, Integer> {
+
+}
