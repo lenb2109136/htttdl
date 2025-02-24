450f942f0d5a2674ad3fe638c0fdc355d0f25210
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 16:33:54 +0700

    Create HuyenController.java

---
+++ src/main/java/com/example/htttdl/controller/HuyenController.java
@@ -0,0 +1,23 @@
+package com.example.htttdl.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.htttdl.response.response;
+import com.example.htttdl.service.QuanHuyenService;
+
+@Controller
+@RequestMapping("/huyen")
+public class HuyenController {
+	@Autowired
+	private QuanHuyenService quanHuyenService;
+	@GetMapping("/getbytinh")
+	public ResponseEntity<response> getall( @RequestParam("id") int id){
+		return new ResponseEntity<response>(new response(HttpStatus.OK,"ok",quanHuyenService.getAllByTinh(id)),HttpStatus.OK);
+	}
+}
