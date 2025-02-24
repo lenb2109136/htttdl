803f9a920851a3e36d4426fc16d23118a19402e6
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 16:34:01 +0700

    Create TinhController.java

---
+++ src/main/java/com/example/htttdl/controller/TinhController.java
@@ -0,0 +1,23 @@
+package com.example.htttdl.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.htttdl.response.response;
+import com.example.htttdl.service.TinhService;
+
+@RestController
+@RequestMapping("/tinh")
+public class TinhController {
+	@Autowired
+	private TinhService tinhService;
+	@GetMapping("/getall")
+	public ResponseEntity<response> getall(){
+		return new ResponseEntity<response>(new response(HttpStatus.OK,"ok",tinhService.getall()),HttpStatus.OK);
+	}
+}
