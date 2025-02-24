b1fc63b1c71eba873a5c232730f6d8ecabefdb73
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:15 +0700

    Create HuyenRepository.java

---
+++ src/main/java/com/example/htttdl/repository/HuyenRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.Huyen;
+
+public interface HuyenRepository extends JpaRepository<Huyen, Integer>{
+
+}
