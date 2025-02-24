2e8bac9fa3a8d8fad223be30017c43bea4ba6938
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:43 +0700

    Create KhachHang.java

---
+++ src/main/java/com/example/htttdl/modal/KhachHang.java
@@ -0,0 +1,67 @@
+package com.example.htttdl.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "KHACHHANG")
+public class KhachHang {
+	@Id
+	@Column(name="KH_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne
+	@JoinColumn(name = "PX_ID")
+	private Xa xa;
+	@Column(name = "KH_TEN")
+	private String ten;
+	@Column(name = "KH_SDT")
+	private String sdt;
+	@Column(name = "KH_DIACHI")
+	private String diaChi;
+	@Column(name = "KH_CCCD")
+	private String cccd;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Xa getXa() {
+		return xa;
+	}
+	public void setXa(Xa xa) {
+		this.xa = xa;
+	}
+	public String getTen() {
+		return ten;
+	}
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	public String getSdt() {
+		return sdt;
+	}
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+	public String getDiaChi() {
+		return diaChi;
+	}
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+	public String getCccd() {
+		return cccd;
+	}
+	public void setCccd(String cccd) {
+		this.cccd = cccd;
+	}
+	
+}
