bab677afd22977be5f27490d7693add3f7e9e247
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:40 +0700

    Create DonHangBuuCuc.java

---
+++ src/main/java/com/example/htttdl/modal/DonHangBuuCuc.java
@@ -0,0 +1,10 @@
+package com.example.htttdl.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DONHANGBUUCUC")
+public class DonHangBuuCuc extends DonHang {
+
+}
