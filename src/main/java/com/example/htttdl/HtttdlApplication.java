8685a79e6af97c20ead3fe2a22a8b6e81a91446e
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 15:31:33 +0700

    Update HtttdlApplication.java

--- src/main/java/com/example/htttdl/HtttdlApplication.java
+++ src/main/java/com/example/htttdl/HtttdlApplication.java
@@ -1,11 +1,15 @@
 package com.example.htttdl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.htttdl.service.HinhThucVanChuyenService;
+
 @SpringBootApplication
 public class HtttdlApplication {
-
+	@Autowired
+	private HinhThucVanChuyenService hinhThucVanChuyenService;
 	public static void main(String[] args) {
 		SpringApplication.run(HtttdlApplication.class, args);
 	}
