3a0180c0a2bfbe1f5adaddb423c08d729803dc72
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:55 +0700

    Create ThoiDiemTrangThaiRepository.java

---
+++ src/main/java/com/example/htttdl/repository/ThoiDiemTrangThaiRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.ThoiDiemTrangThai;
+
+public interface ThoiDiemTrangThaiRepository extends JpaRepository<ThoiDiemTrangThai, Integer> {
+
+}
