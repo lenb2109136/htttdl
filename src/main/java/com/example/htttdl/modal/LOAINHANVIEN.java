8eff0acd774d425f3ca89c654ad93fae7fd29792
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 09:45:59 +0700

    Update LOAINHANVIEN.java

--- src/main/java/com/example/htttdl/modal/LOAINHANVIEN.java
+++ src/main/java/com/example/htttdl/modal/LOAINHANVIEN.java
@@ -1,5 +1,38 @@
 package com.example.htttdl.modal;
 
-public class LOAINHANVIEN {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="LOAINHANVIEN")
+public class LOAINHANVIEN {
+	@Id
+	@Column(name="LNV_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int  id;
+	
+	@Column(name = "LNV_TENLOAINV")
+	private String tenLoai;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTenLoai() {
+		return tenLoai;
+	}
+
+	public void setTenLoai(String tenLoai) {
+		this.tenLoai = tenLoai;
+	}
+	
+	
 }
