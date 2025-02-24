e10f18d22068cf28bfe0d47b0ca06751dd89f648
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:51 +0700

    Create TinhRepository.java

---
+++ src/main/java/com/example/htttdl/repository/TinhRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.Tinh;
+
+public interface TinhRepository extends JpaRepository<Tinh, Integer>{
+
+}
