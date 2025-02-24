a26023675af1f3df8a1cc55f90122cb12f1ae7a3
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:45 +0700

    Create DonHangLayNHan.java

---
+++ src/main/java/com/example/htttdl/modal/DonHangLayNHan.java
@@ -0,0 +1,31 @@
+package com.example.htttdl.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DONHANGLAYNHAN")
+public class DonHangLayNHan extends DonHang {
+	@ManyToOne
+	@JoinColumn(name="PX_ID")
+	private Xa xa;
+	@Column(name = "DHLN_DIACHI")
+	private String diChi;
+	public Xa getXa() {
+		return xa;
+	}
+	public void setXa(Xa xa) {
+		this.xa = xa;
+	}
+	public String getDiChi() {
+		return diChi;
+	}
+	public void setDiChi(String diChi) {
+		this.diChi = diChi;
+	}
+	
+	
+}
