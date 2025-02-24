8004d5e329d10eeb634b655a9cabe6a03f4982a3
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:29:41 +0700

    Update HinhThucVanChuyenRepository.java

--- src/main/java/com/example/htttdl/repository/HinhThucVanChuyenRepository.java
+++ src/main/java/com/example/htttdl/repository/HinhThucVanChuyenRepository.java
@@ -1,9 +1,10 @@
 package com.example.htttdl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.htttdl.modal.HinhThucVanChuyen;
-
+@Repository
 public interface HinhThucVanChuyenRepository extends JpaRepository<HinhThucVanChuyen, Integer> {
 
 }
