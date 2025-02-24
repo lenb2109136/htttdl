9dce23a6a6b5e464adfd500203c2d1b46a34e245
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:11 +0700

    Create LoaiNhanVienRepository.java

---
+++ src/main/java/com/example/htttdl/repository/LoaiNhanVienRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.LOAINHANVIEN;
+
+public interface LoaiNhanVienRepository extends JpaRepository<LOAINHANVIEN, Integer>{
+
+}
