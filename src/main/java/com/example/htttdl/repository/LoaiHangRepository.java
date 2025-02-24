b65d65f9a9bf36326e05d36a6da163fd3114032e
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:09 +0700

    Create LoaiHangRepository.java

---
+++ src/main/java/com/example/htttdl/repository/LoaiHangRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.LoaiHang;
+
+public interface LoaiHangRepository extends JpaRepository<LoaiHang, Integer> {
+
+}
