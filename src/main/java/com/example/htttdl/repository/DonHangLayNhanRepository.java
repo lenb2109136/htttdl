562cdd63cc70be513baaf5432647638d762ad4e1
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:23:19 +0700

    Create DonHangLayNhanRepository.java

---
+++ src/main/java/com/example/htttdl/repository/DonHangLayNhanRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.DonHangLayNHan;
+
+public interface DonHangLayNhanRepository extends JpaRepository<DonHangLayNHan, Integer>{
+
+}
