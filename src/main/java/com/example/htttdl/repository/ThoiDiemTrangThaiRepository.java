01f4ec5d393218137398df3312215c8cb21bbbd6
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:29:15 +0700

    Update ThoiDiemTrangThaiRepository.java

--- src/main/java/com/example/htttdl/repository/ThoiDiemTrangThaiRepository.java
+++ src/main/java/com/example/htttdl/repository/ThoiDiemTrangThaiRepository.java
@@ -1,9 +1,10 @@
 package com.example.htttdl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.htttdl.modal.ThoiDiemTrangThai;
-
+@Repository
 public interface ThoiDiemTrangThaiRepository extends JpaRepository<ThoiDiemTrangThai, Integer> {
 
 }
