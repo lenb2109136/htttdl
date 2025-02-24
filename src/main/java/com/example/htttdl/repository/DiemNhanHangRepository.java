999a0c696cdecd4e4b0a50a1e991209d2237e9b5
lenb2109136 <lenb2109136@student.ctu.edu.vn>
2025-02-24 11:22:02 +0700

    Create DiemNhanHangRepository.java

---
+++ src/main/java/com/example/htttdl/repository/DiemNhanHangRepository.java
@@ -0,0 +1,9 @@
+package com.example.htttdl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.htttdl.modal.DiemNhanHang;
+
+public interface DiemNhanHangRepository extends JpaRepository<DiemNhanHang, Integer>{
+
+}
